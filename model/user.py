1951a605a771fa319b4e18c6f6aa771c133d4681
Dmytro Levkovych <dmytrolevkovych@Dmytros-MBP.fritz.box>
2023-10-18 19:07:05 +0200

    updated at attribute added to user

--- model/user.py
+++ model/user.py
@@ -1,11 +1,12 @@
 
 class User:
-    def __init__(self, firstName, lastName, age, id = None, active = True):
+    def __init__(self, firstName, lastName, age, id = None, active = True, updatedAt = 0):
         self.firstName = firstName
         self.lastName = lastName
         self.age = age
         self.id = id
         self.active = active
+        self.updatedAt = updatedAt
 
     def toJson(self):
-        return {"firstName": self.firstName, "lastName": self.lastName, "age": self.age, "id": self.id}
+        return {"firstName": self.firstName, "lastName": self.lastName, "age": self.age, "id": self.id, "updatedAt": self.updatedAt}
--- repository/user_repo.py
+++ repository/user_repo.py
@@ -23,7 +23,7 @@ def get_all(orders):
     conn = get_conn()
 
     cur = conn.cursor()
-    sql = "select first_name, last_name, age, id, active from user"
+    sql = "select first_name, last_name, age, id, active, updated_at from user"
     if orders != None:
         sql = sql + " order by "
         for i in range(len(orders)):
@@ -35,7 +35,7 @@ def get_all(orders):
 
     rows = cur.fetchall()
 
-    return [User(r[0], r[1], r[2], r[3], r[4]) for r in rows]
+    return [User(r[0], r[1], r[2], r[3], r[4], r[5]) for r in rows]
 
 
 def get_one(id):
@@ -43,12 +43,12 @@ def get_one(id):
 
     cur = conn.cursor()
     user_params = (id,)
-    cur.execute("select first_name, last_name, age, id, active from user where id=? limit 0,1", user_params)
+    cur.execute("select first_name, last_name, age, id, active, updated_at from user where id=? limit 0,1", user_params)
 
     rows = cur.fetchall()
 
     for r in rows:
-        user = User(r[0], r[1], r[2], r[3], r[4])
+        user = User(r[0], r[1], r[2], r[3], r[4], r[5])
         return user.toJson()
 
     return None
@@ -59,11 +59,11 @@ def get_one_by_name(firstName, lastName):
 
     cur = conn.cursor()
     user_params = (firstName, lastName)
-    cur.execute("select first_name, last_name, age, id, active from user where first_name=? and last_name=?", user_params)
+    cur.execute("select first_name, last_name, age, id, active, updated_at from user where first_name=? and last_name=?", user_params)
 
     row = cur.fetchone()
     
-    return None if row == None else User(row[0], row[1], row[2], row[3], row[4])
+    return None if row == None else User(row[0], row[1], row[2], row[3], row[4], row[5])
 
 
 def add(user):
@@ -74,7 +74,7 @@ def add(user):
     id = get_next_id()
 
     user_params = (id, user.firstName, user.lastName, user.age, user.active)
-    cur.execute("insert into user (id, first_name, last_name, age, active) values (?, ?, ?, ?, ?)", user_params)
+    cur.execute("insert into user (id, first_name, last_name, age, active, updated_at) values (?, ?, ?, ?, ?, unixepoch() * 1000)", user_params)
     conn.commit()
     
     return id
@@ -84,7 +84,7 @@ def update(id, user):
 
     cur = conn.cursor()
     user_params = (user.firstName, user.lastName, user.age, user.active, id)
-    cur.execute("update user set first_name=?, last_name=?, age=?, active=? where id=?", user_params)
+    cur.execute("update user set first_name=?, last_name=?, age=?, active=?, updated_at=unixepoch() * 1000 where id=?", user_params)
     conn.commit()
 
     return True
